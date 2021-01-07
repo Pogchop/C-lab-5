8d25aa7e284501e519b3f84c92da5654c46fb3a8
Hieu Phan <64969360+Pogchop@users.noreply.github.com>
2021-01-07 21:13:23 +0700

    Add files via upload

---
+++ Lab5ex2.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab5ex2
+{
+    public sealed class Appraisal
+    {
+        private double bonus;
+        public double Caculatebonus (string designation, int tenure, double salary)
+        {
+            if (designation == "Manager")
+            {
+                if (tenure <= 5)
+                {
+                    bonus = salary * 1.5;
+                }
+                else
+                {
+                    bonus = salary * 2;
+                }
+            }
+            if (designation == "Engineer")
+            {
+                if (tenure <= 5)
+                {
+                    bonus = salary * 1.5;
+                }
+                else
+                {
+                    bonus = salary * 2;
+                }
+            }
+            if (designation == "Technician")
+            {
+                if (tenure <= 3)
+                {
+                    bonus = salary * 0.25;
+                }
+                else if (tenure > 3 && tenure <= 5)
+                {
+                    bonus = salary * 0.5;
+                }
+                else
+                {
+                    bonus = salary * 2;
+                }
+            }
+            return bonus;
+            Console.ReadKey();
+        }
+    }
+}
